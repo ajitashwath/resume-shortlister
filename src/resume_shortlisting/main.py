ab8930d48c77685f6eb3bc789665344f3610b3a7
ajitashwath <ajitashwathr23@lpu.in>
2025-07-23 12:10:21 +0530

    updated

--- src/resume_shortlisting/main.py
+++ src/resume_shortlisting/main.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules["sqlite3.dbapi2"] = sys.modules["pysqlite3.dbapi2"]
+
 def main():
     print("Resume Shortlisting")
 
